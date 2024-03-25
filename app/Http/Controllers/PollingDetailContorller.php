bf45024dda06763f1a96c55cace4d62a25c426f7
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2024-03-25 21:01:47 +0700

    make controller
    semua controller

---
+++ app/Http/Controllers/PollingDetailContorller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PollingDetail;
+use Illuminate\Http\Request;
+
+class PollingDetailContorller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PollingDetail $pollingDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PollingDetail $pollingDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PollingDetail $pollingDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PollingDetail $pollingDetail)
+    {
+        //
+    }
+}
