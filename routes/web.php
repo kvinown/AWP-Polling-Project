38592e094f3e52d5e47e0c99bbe544d9ecc7240e
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2024-03-30 23:49:16 +0700

    route for all controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\KurikulumController;
+use App\Http\Controllers\MataKuliahController;
+use App\Http\Controllers\PollingController;
+use App\Http\Controllers\PollingDetailContorller;
+use App\Http\Controllers\ProgramStudiController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +34,47 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Fakultas
+    Route::get('fakultas-index', [FakultasController::class, 'index'])->name('fakultas-index');
+    Route::get('fakultas-create', [FakultasController::class, 'create'])->name('fakultas-create');
+    Route::get('fakultas-delete/{fakultas}', [FakultasController::class, 'destroy'])->name('fakultas-delete');
+
+    // Kurikulum
+    Route::get('kurikulum-index', [KurikulumController::class, 'index'])->name('kurikulum-index');
+    Route::get('kurikulum-create', [KurikulumController::class, 'create'])->name('kurikulum-create');
+    Route::get('kurikulum-delete/{kurikulum}', [KurikulumController::class, 'destroy'])->name('kurikulum-delete');
+
+    // Mata Kuliah
+    Route::get('matakuliah-index', [MataKuliahController::class, 'index'])->name('matakuliah-index');
+    Route::get('matakuliah-create', [MataKuliahController::class, 'create'])->name('matakuliah-create');
+    Route::get('matakuliah-delete/{mataKuliah}', [MataKuliahController::class, 'destroy'])->name('matakuliah-delete');
+
+    // Polling
+    Route::get('polling-index', [PollingController::class, 'index'])->name('polling-index');
+    Route::get('polling-create', [PollingController::class, 'create'])->name('polling-create');
+    Route::get('polling-delete/{polling}', [PollingController::class, 'destroy'])->name('polling-delete');
+
+    // Polling Detail
+    Route::get('pollingdetail-index', [PollingDetailContorller::class, 'index'])->name('pollingdetail-index');
+    Route::get('pollingdetail-create', [PollingDetailContorller::class, 'create'])->name('pollingdetail-create');
+    Route::get('pollingdetail-delete/{pollingDetail}', [PollingDetailContorller::class, 'destroy'])->name('pollingdetail-delete');
+
+    // Program Studi
+    Route::get('programstudi-index', [ProgramStudiController::class, 'index'])->name('programstudi-index');
+    Route::get('programstudi-create', [ProgramStudiController::class, 'create'])->name('programstudi-create');
+    Route::get('programstudi-delete/{programStudi}', [ProgramStudiController::class, 'destroy'])->name('programstudi-delete');
+
+    // Role
+    Route::get('role-index', [RoleController::class, 'index'])->name('role-index');
+    Route::get('role-create', [RoleController::class, 'create'])->name('role-create');
+    Route::get('role-delete/{role}', [RoleController::class, 'destroy'])->name('role-delete');
+
+    // User
+    Route::get('user-index', [UserController::class, 'index'])->name('user-index');
+    Route::get('user-create', [UserController::class, 'create'])->name('user-create');
+    Route::get('user-delete/{user}', [UserController::class, 'destroy'])->name('user-delete');
+
 });
 
 require __DIR__.'/auth.php';
