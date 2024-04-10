79ec40354d7c90275c120048c4b7d86ad4c9ac6f
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2024-04-10 23:12:27 +0700

    modify navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="nav-height navbar navbar-expand-lg bg-primary text-light navbar-dark">
+<nav class="nav-height navbar navbar-expand-lg bg-primary text-light navbar-dark my-auto">
     <div class="container">
         <a
             class="navbar-brand"
@@ -18,33 +18,39 @@
         <div
             class="collapse navbar-collapse"
             id="navbarNavAltMarkup">
-            <div class="navbar-nav ms-auto">
+            <div class="navbar-nav ms-auto my-auto">
                 <a
-                    class="nav-link active"
+                    class="nav-link active mx-1"
                     aria-current="page"
                     href="{{route('home')}}"
                 >Home</a
                 >
                 <a
-                    class="nav-link active"
+                    class="nav-link active mx-1"
                     href="{{route('matakuliah-index')}}"
                 >Polling Mata Kuliah</a
                 >
                 <a
-                    class="nav-link active"
+                    class="nav-link active mx-1"
                     href="{{route('fakultas-index')}}"
                 >Fakultas</a
                 >
                 <a
-                    class="nav-link active"
+                    class="nav-link active mx-1"
                     href="{{route('programstudi-index')}}"
                 >Program Studi</a
                 >
                 <a
-                    class="nav-link active"
+                    class="nav-link active mx-1"
                     href="#"
                 >Hasil</a
                 >
+                <form id="logoutForm" method="POST" action="{{ route('logout') }}">
+                    @csrf
+                </form>
+                <a href="javascript:void(0)" class="nav-link active btn btn-danger mx-1" onclick="$('#logoutForm').submit()">
+                    <i class="nav-icon fa fa-sign-out"></i> Logout
+                </a>
             </div>
         </div>
     </div>
