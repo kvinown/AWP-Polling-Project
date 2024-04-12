e465bdf9e6901eddc5601c6f13dfdbc5f4a404ae
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2024-04-12 21:06:29 +0700

    kurikulum

--- resources/views/kurikulum/edit.blade.php
+++ resources/views/kurikulum/edit.blade.php
@@ -20,11 +20,11 @@
                             <input class="my-2 form-control" type="text" name="id" id="id" placeholder="ID Kurikulum" required autofocus maxlength="10" readonly value="{{$kur->id}}">
                         </div>
                         <div class="form-group">
-                            <label for="nama">Tahun Kurikulum</label>
+                            <label for="tahun">Tahun Kurikulum</label>
                             <input class="my-2 form-control" type="number" name="tahun" id="tahun" placeholder="Tahun Kurikulum" required autofocus value="{{$kur->tahun}}">
                         </div>
                         <div class="form-group">
-                            <label for="nama">Semester Kurikulum</label>
+                            <label for="semester">Semester Kurikulum</label>
                             <input class="my-2 form-control" type="number" name="semester" id="semester" placeholder="Semester Kurikulum" required autofocus value="{{$kur->semester}}">
                         </div>
                     </div>
